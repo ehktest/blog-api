499349f74a101303ffd8e3786ce254d02dbc9a83
ehktest <ehkarabastest@gmail.com>
2024-03-16 13:00:04 +0300

    dbconn up

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -1,24 +1,12 @@
 "use strict";
 
-
 const mongoose = require("mongoose");
 
-
-
-
-
-const CLUSTER = process.env?.DATABASE_URI || "mongodb:
+const CLUSTER = process.env?.DATABASE_URI;
 const DATABASE = process.env?.DATABASE_NAME || "";
-
-
-
-
-
-
 
 const connectDB = async () => {
   try {
-    
     await mongoose.connect(`${CLUSTER}${DATABASE}`);
     console.log(`*** DB${DATABASE && " " + DATABASE} Connected ***`);
   } catch (err) {
